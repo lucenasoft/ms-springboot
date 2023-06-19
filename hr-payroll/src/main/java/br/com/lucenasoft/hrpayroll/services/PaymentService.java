398dd69592670d6eb1e2f076d482f324b46ed966
lucenasoft <getugbr@gmail.com>
2023-06-19 09:33:36 -0300

    feat: host statically

--- hr-payroll/src/main/java/br/com/lucenasoft/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/lucenasoft/hrpayroll/services/PaymentService.java
@@ -1,11 +1,29 @@
 package br.com.lucenasoft.hrpayroll.services;
 
+import br.com.lucenasoft.hrpayroll.controllers.WorkerModel;
 import br.com.lucenasoft.hrpayroll.models.PaymentModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Value("${hr-worker.host}")
+    private String hrWorkerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public PaymentModel getPayment(Long workerId, int days) {
-        return new PaymentModel("Bob",200.0, days);
+        Map<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("id", String.valueOf(workerId)); //mapa de parametros
+
+        WorkerModel worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", WorkerModel.class, uriVariable);
+        assert worker != null;
+        return new PaymentModel(worker.getName(), worker.getDailyIncome(), days);
     }
 }
