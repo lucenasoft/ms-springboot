05dd41e03f8bd2e5ec44d173b50e8785a3e077f8
lucenasoft <getugbr@gmail.com>
2023-06-19 15:52:55 -0300

    feat: Load balancing with ribbon

--- hr-payroll/src/main/java/br/com/lucenasoft/hrpayroll/Startup.java
+++ hr-payroll/src/main/java/br/com/lucenasoft/hrpayroll/Startup.java
@@ -2,10 +2,12 @@ package br.com.lucenasoft.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@RibbonClient(name = "hr-worker")
 public class Startup {
 	// interface com as assinaturas das requisições que você pode fazer
 
--- hr-worker/src/main/java/br/com/lucenasoft/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/lucenasoft/hrworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.lucenasoft.hrworker.controllers;
 
 import br.com.lucenasoft.hrworker.models.Worker;
 import br.com.lucenasoft.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,11 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repository;
@@ -24,6 +32,9 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Worker>> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Optional<Worker> worker = repository.findById(id);
         return ResponseEntity.ok(worker);
     }
